13a102850091a40aec6b5bb1945279da2d1265e7
Debroop Ghosh <debroopghosh2012@gmail.com>
2024-03-10 13:07:22 +0530

    added final code

---
+++ Todolist_react/src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useTodo } from '../contexts/TodoContext';
+
+function TodoForm() {
+    const [todo, setTodo] = useState("")
+    const {addTodo} = useTodo()
+
+    const add = (e) => {
+      e.preventDefault()
+
+      if (!todo) return
+
+      addTodo({ todo, completed: false})
+      setTodo("")
+    }
+
+  return (
+      <form onSubmit={add}  className="flex">
+          <input
+              type="text"
+              placeholder="Write Todo..."
+              className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white text-black py-1.5" // Changed background and text color
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-200 text-gray-800 shrink-0 hover:bg-green-300"> {/* Changed background and text color */}
+              Add
+          </button>
+      </form>
+  );
+}
+
+export default TodoForm;
